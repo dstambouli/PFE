45d51a5c33c3d7ec57fb467d2987fe149dffb926
Dhia Stambouli <dhia.stde@gmail.com>
2017-12-19 23:40:46 +0100

    -a

--- ANN/simul/data_gen.R
+++ ANN/simul/data_gen.R
@@ -1,10 +1,10 @@
 w1 = 0.5*matrix(rnorm(12),3,4)
 w2 = 0.5*matrix(rnorm(4),4,1)
-
-x1 = rnorm(5000, 0, 1)
-x2 = rnorm(5000, 0.5, 0.30)
-b = rep(1, 5000)
-
+N = 5000
+x1 = rnorm(N, 0, 0.25)
+x2 = rnorm(N, 0.5, 0.30)
+b = rep(1, N)
+epsilon = rnorm(N, 0, 1)
 plot(x1)
 points(x2, col=2)
 
@@ -14,25 +14,30 @@ sig = function(x){
     return ((1+exp(-x))^(-1))
 }
 Y = matrix(nrow = length(b), ncol = 1, 0)
+#for (i in 1:nrow(input)) {
+#    temp1 = sig(input[i,1:3]%*%w1)
+#    temp2 = sig(temp1%*%w2)
+#    Y[i] = temp2
+#}
+
 n1 = 0
 n2 = 0
 for (i in 1:nrow(input)) {
   temp1 = sig(input[i,1:3]%*%w1)
-  temp2 = sig(temp1%*%w2)
-  if(temp2 > 0.515){
+  temp2 = sig(temp1%*%w2) + epsilon[i]
+  if(temp2 > 0.45){
     Y[i] = 1 
     n1 = n1 +1
   }
-  if(temp2 <= 0.515){
+  if(temp2 <= 0.45){
     Y[i] = 0
     n2 = n2 + 1
   }
-  
 }
 
 
 
-filename <- file('data.csv', open = "w")
+filename <- file('data1.csv', open = "w")
 for(i in 1:nrow(input)){
   writeLines(paste(c(input[i,1:3], Y[i]), collapse = ";"), filename)
 }
